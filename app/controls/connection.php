67ff57d73bf8dea79cdcb5ad6e5e39a330eb2549
Pedro Santos de Oliveira <117773287+Peredo2006@users.noreply.github.com>
2025-11-11 14:28:30 -0300

    Add files via upload

---
+++ app/controls/connection.php
@@ -0,0 +1,7 @@
+<?php
+$server= "40.160.64.65";
+$user= "hostdeprojetos_6tech";
+$password= "FV0(KqV11XvptPJx";
+$dbname= "hostdeprojetos_xavierspapelaria";
+$conn = mysqli_connect($server, $user, $password, $dbname);
+?>
